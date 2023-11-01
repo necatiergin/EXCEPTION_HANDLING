fc943684584397143fb2a0baa0b4c6cf5a2371e5
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-11-01 13:04:02 +0300

    Update nested_exception_03.cpp

--- nested_exception/nested_exception_03.cpp
+++ nested_exception/nested_exception_03.cpp
@@ -1,42 +1,42 @@
-soon
+#include <iostream>       
+#include <exception>      
+#include <stdexcept>      
 
-<!--
-/ nested_exception example
-#include <iostream>       // std::cerr
-#include <exception>      // std::exception, std::throw_with_nested, std::rethrow_if_nested
-#include <stdexcept>      // std::logic_error
+// recursively print exception what's string:
 
-// recursively print exception whats:
-void print_what (const std::exception& e) {
-  std::cerr << e.what() << '\n';
-  try {
-    std::rethrow_if_nested(e);
-  } catch (const std::exception& nested) {
-    std::cerr << "nested: ";
-    print_what(nested);
-  }
+void print_exception(const std::exception& e) {
+    std::cerr << e.what() << '\n';
+    try {
+        std::rethrow_if_nested(e);
+    }
+    catch (const std::exception& nested) {
+        std::cerr << "nested: ";
+        print_exception(nested);
+    }
+}
+
+void bar()
+{
+    throw std::runtime_error("exception from bar");
 }
 
 // throws an exception nested in another:
-void throw_nested() {
-  try {
-    throw std::logic_error ("first");
-  } catch (const std::exception& e) {
-    std::throw_with_nested(std::logic_error("second"));
-  }
+void foo() 
+{
+    try {
+        bar();
+    }
+    catch (const std::exception&) {
+        std::throw_with_nested(std::runtime_error("exception from foo"));
+    }
 }
 
-int main () {
-  try {
-    throw_nested();
-  } catch (std::exception& e) {
-    print_what(e);
-  }
-
-  return 0;
+int main() 
+{
+    try {
+        foo();
+    }
+    catch (std::exception& e) {
+        print_exception(e);
+    }
 }
-
-
--->
-
-

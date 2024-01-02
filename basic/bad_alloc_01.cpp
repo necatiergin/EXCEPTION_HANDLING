8a828184b797904382b63f239d74655d3546ae7f
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-01-02 18:53:24 +0300

    Create bad_alloc_01.cpp

---
+++ basic/bad_alloc_01.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <exception>
+
+class Buffer {
+	constexpr static auto size{ 1'024u* 1'024u};
+	char mbuffer[size];
+};
+
+int main()
+{
+	std::vector<Buffer*> bvec;
+
+	try {
+		for (;;) {
+			bvec.push_back(new Buffer);
+			std::cout << '.';
+		}
+	}
+	catch (const std::bad_alloc& ex) {
+		std::cout << "\nexception caught : " << ex.what() << "\n";
+		std::cout << "size = " << bvec.size() << "\n";
+	}
+}

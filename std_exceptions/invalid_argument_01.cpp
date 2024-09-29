13457d4fb7bb165db0796c8fe598c5ed6af04657
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-09-29 18:14:23 +0300

    Create invalid_argument_01.cpp

---
+++ std_exceptions/invalid_argument_01.cpp
@@ -0,0 +1,36 @@
+// from cppreference.com
+
+#include <bitset>
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+int main()
+{
+    try
+    {
+        std::bitset<4>{"012"}; // Throws: only '0' or '1' expected
+    }
+    catch (std::invalid_argument const& ex)
+    {
+        std::cout << "#1: " << ex.what() << '\n';
+    }
+
+    try
+    {
+        [[maybe_unused]] int f = std::stoi("ABBA"); // Throws: no conversion
+    }
+    catch (std::invalid_argument const& ex)
+    {
+        std::cout << "#2: " << ex.what() << '\n';
+    }
+
+    try
+    {
+        [[maybe_unused]] float f = std::stof("(3.14)"); // Throws: no conversion
+    }
+    catch (std::invalid_argument const& ex)
+    {
+        std::cout << "#3: " << ex.what() << '\n';
+    }
+}

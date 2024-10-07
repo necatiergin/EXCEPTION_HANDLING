39ee30c84d11c4e3a9593aa815ed240a3815fa52
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-10-07 17:51:30 +0300

    Update noexcept_specifier_04.cpp

--- noexcept/noexcept_specifier_04.cpp
+++ noexcept/noexcept_specifier_04.cpp
@@ -6,9 +6,9 @@ class Myclass {
 
 int main()
 {
-	const auto b1 = std::is_nothrow_default_constructible_v<Myclass>;
-	const auto b2 = std::is_nothrow_copy_constructible_v<Myclass>;
-	const auto b3 = std::is_nothrow_move_constructible_v<Myclass>;
-	const auto b4 = std::is_nothrow_copy_assignable_v<Myclass>;
-	const auto b5 = std::is_nothrow_move_assignable_v<Myclass>;
+	constexpr auto b1 = std::is_nothrow_default_constructible_v<Myclass>;
+	constexpr auto b2 = std::is_nothrow_copy_constructible_v<Myclass>;
+	constexpr auto b3 = std::is_nothrow_move_constructible_v<Myclass>;
+	constexpr auto b4 = std::is_nothrow_copy_assignable_v<Myclass>;
+	constexpr auto b5 = std::is_nothrow_move_assignable_v<Myclass>;
 }

0ebd1b2374d596bb687ed1ff9e57d73a27a21e4c
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-12 17:38:34 +0300

    Update construction_tracker_01.cpp

--- idioms/construction_tracker_01.cpp
+++ idioms/construction_tracker_01.cpp
@@ -5,7 +5,7 @@
 struct X {
     X(const char*)
     {
-        //throw std::runtime_error("X Error");
+        throw std::runtime_error("X Error");
     }
 };
 
@@ -24,16 +24,16 @@ struct Z {
 };
 
 class Nec {
-    X ma;
-    Y mb;
-    Z mc;
+    X mx;
+    Y my;
+    Z mz;
     enum TrackerType { NONE, ONE, TWO, THREE };
 public:
     Nec(TrackerType tracker = NONE)
         try    // X constructor try block.
-        : ma((tracker = ONE, "hello"))  // Can throw std::exception
-        , mb((tracker = TWO, "world"))   // Can throw std::exception
-        , mc((tracker = THREE, "world")) // Can throw std::exception
+        : mx((tracker = ONE, "hello"))  // Can throw std::exception
+        , my((tracker = TWO, "world"))   // Can throw std::exception
+        , mz((tracker = THREE, "world")) // Can throw std::exception
     {
         assert(tracker == THREE);
         // ... constructor body ...
